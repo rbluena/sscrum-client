98146aab233c743e96649ea4fb4863ae8b46a326
Rabii Luena <itisrabii@gmail.com>
2020-10-31 12:07:11 +0300

    * fixing tailwind not working with storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,42 @@
+const path = require('path');
+
 module.exports = {
   stories: [
-    "../src/components/**/*.stories.mdx",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    '../src/components/**/*.stories.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        // Loader for webpack to process CSS with PostCSS
+        {
+          loader: 'postcss-loader',
+          options: {
+            /* 
+              Enable Source Maps
+             */
+            sourceMap: true,
+            /*
+              Set postcss.config.js config path && ctx 
+             */
+            config: {
+              path: './.storybook/',
+            },
+          },
+        },
+      ],
+
+      include: path.resolve(__dirname, '../'),
+    });
+
+    // Return the altered config
+    return config;
+  },
 };
