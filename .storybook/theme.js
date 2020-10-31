9670b81977523aa0e39545f47a6192eb5ca2cf5c
Rabii Luena <itisrabii@gmail.com>
2020-10-31 12:08:08 +0300

    * storybook theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,19 +1,7 @@
-// https://storybook.js.org/docs/react/configure/theming
-import { create } from "@storybook/theming/create";
+import { create } from '@storybook/theming/create';
 
 export default create({
-  base: "light",
-  brandTitle: "Rabii Luena",
-  brandUrl: "https://rabiiluena.com",
-
-  colorPrimary: "white",
-
-  // UI
-  appBg: "#63C7FF",
-  appBorderRadius: 4,
-
-  // Toolbar
-  barBg: "#2643b9",
-  barTextColor: "#DDDDDD",
-  barSelectedColor: "#FFFFFF",
+  base: 'light',
+  brandTitle: 'SSCRUM',
+  brandUrl: 'https://sscrum.com',
 });
